c17feefd538d6d62a1f6edf49981765883509046
Anatoliy0707 <anatoliy_sukhanov@mail.ru>
2021-11-07 18:46:52 +0500

    finished 6 dz

--- src/p2-homeworks/h6/HW6.tsx
+++ src/p2-homeworks/h6/HW6.tsx
@@ -2,6 +2,7 @@ import React, {useState} from 'react'
 import SuperEditableSpan from './common/c4-SuperEditableSpan/SuperEditableSpan'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
 import {restoreState, saveState} from './localStorage/localStorage'
+import s from "./HW6.module.css"
 
 function HW6() {
     const [value, setValue] = useState<string>('')
@@ -10,11 +11,11 @@ function HW6() {
         saveState<string>('editable-span-value', value)
     }
     const restore = () => {
-        // setValue()
+         setValue(restoreState<string>('editable-span-value', ''))
     }
 
     return (
-        <div>
+        <div className={s.container}>
             <hr/>
             homeworks 6
 
